b1128ad1181636555c921c9727ffb88b93e82098
jc347791 <kevin.hinds1@my.jcu.edu.au>
2019-04-03 15:48:49 +1000

    Menu complete

--- assignment1.py
+++ assignment1.py
@@ -1,14 +1,32 @@
 """
 Replace the contents of this module docstring with your own details
-Name:
-Date started:
+Name: Kevin Hinds
+Date started: 29/03/2019
 GitHub URL:
 """
+menu = """L - List places
+   A - Add new place
+   M - Mark a place as visited
+   Q - Quit"""
 
 
 def main():
-    print("Travel Tracker 1.0 - by <Your Name>")
+    print("Travel Tracker 1.0 - by Kevin Hinds")
+    print(menu)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "L":
+            print("list")
+        elif choice == "A":
+            print("add")
+        elif choice == "M":
+            print("mark")
+        else:
+            print("Invalid menu choice")
+        print(menu)
+        choice = input(">>> ").upper()
+# print("{} places saved to places.csv".format(total_number_places_visited) )
+    print("Have a nice day :)")
 
 
-if __name__ == '__main__':
-    main()
+main()
